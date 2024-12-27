ff0bf53763ec126fef282c885afff5df34f5d991
Arun Khatri <akcs4444@gmail.com>
2024-12-27 16:49:17 +0545

    add: project links

--- src/components/Porject_data.js
+++ src/components/Porject_data.js
@@ -1,17 +1,20 @@
 export const applications = [
   {
-    imageSrc: "images/notebook.jpg",
-    title: "iNotebook",
+    imageSrc:
+      "https://static.vecteezy.com/system/resources/thumbnails/035/947/339/small_2x/blog-3d-illustration-icon-png.png",
+    title: "Blogspurr (blogging webapp)",
     description:
-      "This is a webapp where you can store your notes privately. All notes are kept private. You can create an account and store your notes in it safely.",
+      "This is a fullstack webapp where you can share your thoughts, ideas and experiences with the world.",
     buttonLabel: "Check this out",
+    link: "https://blogs-spurr-zj3s.vercel.app",
   },
   {
-    imageSrc: "images/chat (1).svg",
+    imageSrc: "https://cdn-icons-png.flaticon.com/512/4564/4564089.png",
     title: "Chat App",
     description:
-      "I used react and firebase (for authentication and data storage) in the app. It is like superchat in youtube.",
+      "This is a chat webapp made using react and firebase. It is a simple group chat app.",
     buttonLabel: "Check this out",
+    link: "https://kuragaram.vercel.app/",
   },
   {
     imageSrc: "images/git.png",
@@ -22,11 +25,13 @@ export const applications = [
     link: "https://searchgithubbyarun.vercel.app/",
   },
   {
-    imageSrc: "src/images/muvi.jpg",
+    imageSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0mWAR9cShYU0XR7T_rRzvhiykXDddUKQqQ0yxelWn6dqWEFuianS6pbdu87fW2b4SrTU&usqp=CAU",
     title: "Movie WebApp",
     description:
       "A movie website created by using React and TMDB api. It has features of searching, browsing various categories and getting individual movie details.",
     buttonLabel: "Check this out",
+    link: "https://moobiedoobie.vercel.app/",
   },
   {
     imageSrc: "images/linkshort.png",
@@ -34,5 +39,6 @@ export const applications = [
     description:
       "This is a link shortening webapp that is made using shorcode API.",
     buttonLabel: "Check this out",
+    link: "https://linkshortenerbyarun.vercel.app/",
   },
 ];
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,48 +1,60 @@
-
-import React, { useState } from 'react'
-import { motion } from 'framer-motion'
-import { applications } from './Porject_data'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { applications } from "./Porject_data";
 const Projects = () => {
-    const [isParentAnimated, setIsParentAnimated] = useState(false)
-    let duration = 0;
-    return (
-        <>
-            <motion.section
-                class="light overflow-hidden">
-                <h2>👩🏽‍🚀 Projects</h2>
+  const [isParentAnimated, setIsParentAnimated] = useState(false);
+  let duration = 0;
+  return (
+    <>
+      <motion.section class="light overflow-hidden">
+        <h2>👩🏽‍🚀 Projects</h2>
 
-                <p>
-                    These are some applications and projects that I've worked on. I've
-                    mainly used react for frontend and firebase for backend in these
-                    projects. Feel free to check them out below and find the source codes
-                    in my github.
-                </p>
+        <p>
+          These are some applications and projects that I've worked on. I've
+          mainly used react for frontend and firebase for backend in these
+          projects. Feel free to check them out below.
+          <br />
+          <span className="mt-5">
+            To view all my other projects, please visit my github by clicking{" "}
+            <a
+              className="underline text-blue-300 hover:text-blue-100"
+              href="https://github.com/errunlee"
+              target="_blank"
+            >
+              here.
+            </a>
+          </span>
+        </p>
 
-                <div class="project_cards">
-                    {
-                        applications.map((app, ind) => {
-                            const { imageSrc, title, description } = app
-                            return (
-                                <motion.div key={ind}
-                                    initial={{ scale: 0 }}
-                                    whileInView={{ scale: 1 }}
-                                    transition={{ duration: 0.4, delay: duration += 0.2 }}
-                                    viewport={{ once: true }}
-                                    class="box reveal">
-                                    <img src={imageSrc} alt="note image" />
-                                    <h2>{title}</h2>
-                                    <p>
-                                        {description}
-                                    </p>
-                                    <button id="notebook">Check this out</button>
-                                </motion.div>
-                            )
-                        })
-                    }
-                </div>
-            </motion.section >
-        </>
-    )
-}
+        <div class="project_cards">
+          {applications.map((app, ind) => {
+            const { imageSrc, title, description, link } = app;
+            return (
+              <motion.div
+                key={ind}
+                initial={{ scale: 0 }}
+                whileInView={{ scale: 1 }}
+                transition={{ duration: 0.4, delay: (duration += 0.2) }}
+                viewport={{ once: true }}
+                class="box reveal"
+              >
+                <img src={imageSrc} alt="note image" />
+                <h2>{title}</h2>
+                <p>{description}</p>
+                <a
+                  className="px-3 py-2 border mt-5 hover:text-black hover:bg-white"
+                  href={link}
+                  target="_blank"
+                >
+                  Check this out
+                </a>
+              </motion.div>
+            );
+          })}
+        </div>
+      </motion.section>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
